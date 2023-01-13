31b94dcb2e58a748365d038b2e1233a2cedc7ca1
Tyler <tylerbarrett2010+github@gmail.com>
2023-01-13 18:45:13 -0500

    We now maintain the IP of each agent in the swarm

--- connected_agents_monitor.py
+++ connected_agents_monitor.py
@@ -8,6 +8,9 @@ import datetime
 import json
 
 
+SERVER_BIND_IP = "127.0.0.1"
+
+
 class ConnectedAgentsMonitor:
     def __init__(self, heartbeat_interface_port, agents_list_port):
         self.active_agents = {}
@@ -24,13 +27,13 @@ class ConnectedAgentsMonitor:
     def run_server(self, bind_port, incoming_data_handler):
         broker_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         broker_server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        broker_server.bind(("127.0.0.1", bind_port))
+        broker_server.bind((SERVER_BIND_IP, bind_port))
         broker_server.listen()
         conn, addr = broker_server.accept()
 
         while True:
             data = conn.recv(2 ** 16)  # TODO - put in a layer similar to TCP where we join our packets together?
-            response = incoming_data_handler(data)
+            response = incoming_data_handler(data, addr)
 
             if response:
                 conn.send(response)
@@ -39,22 +42,24 @@ class ConnectedAgentsMonitor:
             conn, addr = broker_server.accept()
 
 
-    def handle_incoming_heartbeat(self, msg_str):
+    def handle_incoming_heartbeat(self, msg_str, addr):
         incoming_heartbeat = json.loads(msg_str)
         agent_id = incoming_heartbeat["id"]
         timestamp = datetime.datetime.now()
-        self.active_agents[agent_id] = timestamp
+        self.active_agents[agent_id] = {"timestamp": timestamp, "ip": addr[0]}
 
 
-    def handle_request_for_agents(self, _):
+    def handle_request_for_agents(self, msg_str, addr):
         now = datetime.datetime.now()
 
         active_agents = []
         old_active_agents = list(self.active_agents.keys())
         for agent_id in old_active_agents:
-            last_timestamp_time = self.active_agents[agent_id]
+            agent_data = self.active_agents[agent_id]
+            last_timestamp_time = agent_data["timestamp"]
+            agent_ip = agent_data["ip"]
             if now - last_timestamp_time < datetime.timedelta(seconds=10):
-                active_agents.append(agent_id)
+                active_agents.append((agent_ip, agent_id))
             else:
                 del self.active_agents[agent_id]
 
